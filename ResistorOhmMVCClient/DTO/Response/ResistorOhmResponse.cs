ba48dbc34768b0ae8c828e2fb40d7fccae5ccc57
Sinha, Raushan (Contractor) <rsinha245@cable.comcast.com>
2018-04-24 03:08:48 -0400

    first commit

---
+++ ResistorOhmMVCClient/DTO/Response/ResistorOhmResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResistorOhmMVCClient.DTO.Response
+{
+    public class ResistorOhmResponse
+    {
+        public int Ohm { get; set; }
+        public bool Success { get; set; }
+        public string Error { get; set; }
+    }
+}
